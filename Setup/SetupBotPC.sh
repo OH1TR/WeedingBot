577aa212e1d425f8efe95827eadcd874119abe7f
Tommi Rouvali <tommi@rouvali.com>
2018-02-25 14:35:52 +0200

    Update SetupBotPC.sh

--- Setup/SetupBotPC.sh
+++ Setup/SetupBotPC.sh
@@ -3,32 +3,32 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install apptitude
+sudo apt-get -y install apptitude
 
 sudo apt-get remove x264 libx264-dev
 
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm
-sudo apt-get install git gfortran
-sudo apt-get install libjpeg8-dev libpng-dev
+sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get -y install git gfortran
+sudo apt-get -y install libjpeg8-dev libpng-dev
 
-sudo apt-get install libtiff5-dev
+sudo apt-get -y install libtiff5-dev
 
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
 
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+sudo apt-get -y install libxine2-dev libv4l-dev
+sudo apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get -y install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get -y install libvorbis-dev libxvidcore-dev
+sudo apt-get -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get -y install x264 v4l-utils
 
-sudo apt-get install libprotobuf-dev protobuf-compiler
-sudo apt-get install libgoogle-glog-dev libgflags-dev
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get -y install libprotobuf-dev protobuf-compiler
+sudo apt-get -y install libgoogle-glog-dev libgflags-dev
+sudo apt-get -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
 
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get -y install python-dev python-pip python3-dev python3-pip
 
 sudo -H pip install numpy scipy matplotlib scikit-image scikit-learn ipython
 sudo -H pip3 install numpy scipy matplotlib scikit-image scikit-learn ipython
