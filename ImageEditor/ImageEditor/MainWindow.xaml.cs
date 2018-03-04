52793044336edad0f8761698b391ae4741ba1516
Tommi Rouvali <tommi@rouvali.com>
2018-03-04 20:55:02 +0200

    Added image editting tool.(that cannot edit anything yet)

---
+++ ImageEditor/ImageEditor/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string _filename;
+        TRImage _image;
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|GIF Files (*.gif)|*.gif";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                _filename = dlg.FileName;
+                Filename.Text = _filename;
+                LoadImageToView();
+            }
+        }
+  
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadImageToView()
+        {
+            _image = new TRImage();
+            _image.LoadImage(_filename);
+            //Image.Source = ImageSourceForBitmap(_image.OriginalImage);
+            _image.CalculateCatecoryMask(-0.05f);
+            Image.Source = ImageSourceForBitmap(_image.DrawMask(TRImage.MaskDrawType.DimOthers,1));
+        }
+
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceForBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+    }
+}
---
+++ ImageEditor/ImageEditor/TRImage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ImageEditor
+{
+    class TRImage
+    {
+        public enum MaskDrawType { SelectedOnly, DimOthers }
+
+        public Bitmap OriginalImage;
+        public float[,] ExcessiveGreen;
+        public int[,] CatecoryMask;
+
+
+        public void LoadImage(string path)
+        {
+            OriginalImage = new Bitmap(path);
+            CalculateExcessiveGreen();
+        }
+
+        /// <summary>
+        /// Calculate excessive green minus excessive red 
+        /// </summary>
+        void CalculateExcessiveGreen()
+        {
+            ExcessiveGreen = new float[OriginalImage.Width, OriginalImage.Height];
+
+            for (int x = 0; x < OriginalImage.Width; x++)
+            {
+                for (int y = 0; y < OriginalImage.Height; y++)
+                {
+                    var px = OriginalImage.GetPixel(x, y);
+                    float r = px.R;
+                    float g = px.G;
+                    float b = px.B;
+
+                    if ((r + b + g) > 0)
+                        ExcessiveGreen[x, y] = (3 * g - 2.4f * r - b) / (r + b + g);
+                    else
+                        ExcessiveGreen[x, y] = 0;
+                }
+            }
+        }
+
+        public void CalculateCatecoryMask(float stresshold)
+        {
+            CatecoryMask = new int[OriginalImage.Width, OriginalImage.Height];
+
+            for (int x = 0; x < OriginalImage.Width; x++)
+            {
+                for (int y = 0; y < OriginalImage.Height; y++)
+                {
+                    CatecoryMask[x, y] = ExcessiveGreen[x, y] > stresshold ? 1 : 0;
+                }
+            }
+        }
+
+        public Bitmap DrawMask(MaskDrawType type, int category)
+        {
+            Bitmap retval = new Bitmap(OriginalImage);
+
+            for (int x = 0; x < OriginalImage.Width; x++)
+            {
+                for (int y = 0; y < OriginalImage.Height; y++)
+                {
+                    switch (type)
+                    {
+                        case MaskDrawType.SelectedOnly:
+                            if (CatecoryMask[x, y] == category)
+                                retval.SetPixel(x, y, OriginalImage.GetPixel(x, y));
+                            else
+                                retval.SetPixel(x, y, Color.Black);
+                            break;
+
+                        case MaskDrawType.DimOthers:
+                            if (CatecoryMask[x, y] == category)
+                                retval.SetPixel(x, y, OriginalImage.GetPixel(x, y));
+                            else
+                            {
+                                Color px = OriginalImage.GetPixel(x, y);
+                                Color newpx = Color.FromArgb(255, px.R / 2, px.B / 2, px.G / 2);
+                                retval.SetPixel(x, y, newpx);
+                            }
+                            break;
+                    }
+                }
+            }
+            return (retval);
+        }
+    }
+}
+
